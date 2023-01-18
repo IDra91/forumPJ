ebc8cb72f6048d1fefe1e56000ff9cd1dfb8f601
Manuel_Rivera <manuel.contactos91@protonmail.com>
2023-01-17 18:59:09 -0600

    Klein veränderung
    Modificando el CSS y perfeccionando la vista.

--- views/session-view.php
+++ views/session-view.php
@@ -3,11 +3,11 @@
     require 'logic/mstr_categorias.php';
     while($mostar=mysqli_fetch_array($result)){
     ?>
-        <div class="content_title" name="content_title">
+        <div class="navbar-default" name="navbar-default">
         <a class="navlink-title" href="<?php echo SERVERURL; ?>cat/"><?php echo $mostar['nombre']?></a> 
             
     </div>
-    <div class="content_description" name="content_description">
+    <div class="contenido" name="contenido">
         <?php echo $mostar['descripcion']?>
    
     </div>
